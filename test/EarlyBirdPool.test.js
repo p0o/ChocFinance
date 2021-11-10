cdaa057765b880776d8adecba710ece4abac0871
p0oker <p0oker@protonmail.com>
2021-11-10 20:15:10 +0800

    fix reward calculations

--- test/EarlyBirdPool.test.js
+++ test/EarlyBirdPool.test.js
@@ -5,6 +5,8 @@ const {
   isCallTrace,
 } = require("hardhat/internal/hardhat-network/stack-traces/message-trace")
 
+const { utils, BigNumber } = ethers
+
 describe("EarlyBirdPool", function() {
   before(async function() {
     this.signers = await ethers.getSigners()
@@ -25,18 +27,18 @@ describe("EarlyBirdPool", function() {
   beforeEach(async function() {
     this.choc = await this.ChocToken.deploy()
     await this.choc.deployed()
-    this.earlyBird = await this.EarlyBirdPool.deploy(this.choc.address, 100)
+    this.earlyBird = await this.EarlyBirdPool.deploy(this.choc.address, 432000)
     await this.earlyBird.deployed()
     // storing the start block for later
     this.startBlock = await ethers.provider.getBlockNumber()
 
-    // make EarlyBird the owner of the Choc
-    await this.choc.transferOwnership(this.earlyBird.address)
+    // make EarlyBird the minter of the Choc
+    await this.choc.setMinter(this.earlyBird.address)
   })
 
   it("should set the ending block number correctly", async function() {
     const block = await this.earlyBird.endBlock()
-    expect(block).to.equal(100 + this.startBlock) // 1 block is passed since choc.transferOwnership
+    expect(block).to.equal(432000 + this.startBlock) // 1 block is passed since choc.transferOwnership
   })
 
   it("should allow users to deposit", async function() {
@@ -50,34 +52,58 @@ describe("EarlyBirdPool", function() {
 
   it("should calculate choc rewards correctly", async function() {
     const bobWallet = this.earlyBird.connect(this.bob)
+
     await bobWallet.deposit({
       from: this.bob.address,
-      value: ethers.utils.parseEther("10"),
+      value: ethers.utils.parseEther("1"),
     })
+
     let currentBlock = await ethers.provider.getBlockNumber()
-    const rewardsPerHour = 100
+
+    const chocsPerHour = 10
+    const blockDiff = 10
     const blocksPerHour = 600
 
-    const pending1 = await bobWallet.pendingChoc()
-    expect(pending1.toString()).to.equal("0")
-
     // in 10 blocks (~1 min)
-    await time.advanceBlockTo(currentBlock + 10)
+    await time.advanceBlockTo(currentBlock + blockDiff)
 
     const pending2 = await bobWallet.pendingChoc()
-    let rewardFor10Blocks = Math.floor(
-      (10 * rewardsPerHour * 10) / blocksPerHour
-    )
-    rewardFor10Blocks = rewardFor10Blocks - Math.floor(rewardFor10Blocks / 10)
-    expect(pending2.toNumber()).to.equal(rewardFor10Blocks)
+    let rewardFor10Blocks = utils
+      .parseUnits("1", 18)
+      .mul(BigNumber.from(blockDiff))
+      .mul(BigNumber.from(chocsPerHour))
+      .div(BigNumber.from(blocksPerHour))
+
+    expect(pending2).to.equal(rewardFor10Blocks.toString())
 
     currentBlock = await ethers.provider.getBlockNumber()
 
     // In 600 blocks (~one hour)
     await time.advanceBlockTo(currentBlock + 590)
     const pending3 = await bobWallet.pendingChoc()
-    // 10 BCH staked in one hour should have a reward of 900 CHOCs
-    expect(pending3.toNumber()).to.equal(900)
+    // 1 BCH staked in one hour should have a reward of 10 CHOCs
+    expect(pending3).to.equal(utils.parseUnits("10", 18))
+  })
+
+  it("should calculate choc rewards for stakes below 1 BCH correctly", async function() {
+    const bobWallet = this.earlyBird.connect(this.bob)
+    await bobWallet.deposit({
+      from: this.bob.address,
+      value: ethers.utils.parseEther("0.0001"),
+    })
+    let currentBlock = await ethers.provider.getBlockNumber()
+    const blocksPerHour = 600
+
+    const pending1 = await bobWallet.pendingChoc()
+    expect(pending1.toString()).to.equal("0")
+
+    currentBlock = await ethers.provider.getBlockNumber()
+
+    // In 600 blocks (~one hour)
+    await time.advanceBlockTo(currentBlock + blocksPerHour)
+    const pending2 = await bobWallet.pendingChoc()
+    // 0.0001 BCH staked in one hour should have a reward of 0.001 CHOCs
+    expect(pending2).to.equal(utils.parseUnits("0.001", 18))
   })
 
   it("should allow the staker to exit with the right amount", async function() {
@@ -109,7 +135,5 @@ describe("EarlyBirdPool", function() {
       // exact amount is not predictable due to gas costs
       parseFloat(ethers.utils.formatEther(balanceAfterDeposit)) + 3.5
     )
-
-    // TODO: calculate yields
   })
 })
